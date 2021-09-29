41a66c95492f43063ccc96f7b2b45a82832ff2fc
vitor <vitorsbsilva@gmail.com>
2021-09-29 18:31:26 -0300

    Requisito 9 feito

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -42,14 +42,23 @@ Retorno:
 */
 
 describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
-  it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
-    fail('Teste vazio!');
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
-    // Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
-    // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
-    // Teste se os parâmetros não são Case Sensitive.
-    // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
+  it('Verifica se há o retorno esperado para Arya Stark from Winterfell', () => {
+    expect(getCharacter('Arya')).toStrictEqual({
+      name: 'Arya Stark',
+      class: 'Rogue',
+      phrases: [ 'Not today', 'A girl has no name.' ]
+    })
+  });
+
+  it('Verificação para nenhum parâmetro', () => {
+    expect(getCharacter()).toBe(undefined);
+  });
+
+  it('Verificação para case sensitive', () => {
+    expect(getCharacter('aRyA')).toStrictEqual({
+      name: 'Arya Stark',
+      class: 'Rogue',
+      phrases: [ 'Not today', 'A girl has no name.' ]
+    })
   });
 });
